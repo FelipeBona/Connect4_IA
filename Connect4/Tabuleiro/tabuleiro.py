f260158d6bc2101641313646bd8ea8c35b98b5bc
Mateus Maas <maas.mateus@gmail.com>
2024-05-04 15:07:18 -0300

    Alterações na inicialização da classe de Tabuleiro e criação do arquivo main para rodar o projeto

--- Connect4/Tabuleiro/tabuleiro.py
+++ Connect4/Tabuleiro/tabuleiro.py
@@ -1,3 +1,30 @@
 class Tabuleiro:
-  def __init__(self) -> None:
-    pass
+  
+  # A posição do grid deverá ser acessada da seguinte forma:
+  # grid[linha][coluna]
+
+  grid = list()
+
+  larguraTabuleiro = 7
+  alturaTabuleiro = 6
+  
+  def __init__(self):
+
+    for i in range(self.larguraTabuleiro): 
+      self.grid.append([])
+      for j in range(self.alturaTabuleiro):
+        self.grid[i].append(0)
+
+    return self;
+  
+  def colocar_peca(self, coluna, tipoPeca):
+
+    for linha in self.grid:
+      if (linha[coluna]) == 0:
+        linha[coluna] = tipoPeca
+        return
+      
+    else:
+      raise Exception("Não é mais possível colocar peças nessa coluna")
+    
+  
---
+++ Connect4/main.py
@@ -0,0 +1,12 @@
+from InterfaceGrafica.interfaceGrafica import *
+import Tabuleiro.tabuleiro as t
+
+def main():
+
+  tabuleiro = t.Tabuleiro()
+
+  connect_4(tabuleiro)
+
+
+if __name__ == '__main__':
+  main()
